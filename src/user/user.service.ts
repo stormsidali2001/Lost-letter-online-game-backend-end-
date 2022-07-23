bcaa5163b1bbb422b9a15fe49459bda12397a620
stormsidali2001 <assoulsidali@gmail.com>
2022-07-23 11:09:33 +0200

    sign tokens

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,13 +4,19 @@ import { User } from "src/mongoose/user.schema";
 import { createUserDto, LoginUserDto } from "./user.dto";
 import { UserRepository } from "./user.repository";
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import { Tokens } from "./types/tokens.type";
+import { JwtPayload } from "./types/jwtPayload.type";
 
 
 @Injectable()
 export class UserService{
     private readonly logger = new Logger(UserService.name);
     constructor(
-        private readonly userRepository:UserRepository
+        private readonly userRepository:UserRepository,
+        private jwtService: JwtService,
+        private config: ConfigService,
     ){}
 
     async getUserById(id:string){
@@ -57,7 +63,28 @@ export class UserService{
             throw new UnauthorizedException("Invalid credentials");
         }
     }
-
+    async getTokens(userId: number, email: string): Promise<Tokens> {
+        const jwtPayload: JwtPayload = {
+          sub: userId,
+          email: email,
+        };
+    
+        const [access_token, refresh_token] = await Promise.all([
+          this.jwtService.signAsync(jwtPayload, {
+            secret: this.config.get<string>('ACCESS_TOKEN_SECRET'),
+            expiresIn: '15m',
+          }),
+          this.jwtService.signAsync(jwtPayload, {
+            secret: this.config.get<string>('REFRESH_TOKEN_SECRET'),
+            expiresIn: '7d',
+          }),
+        ]);
+    
+        return {
+          access_token,
+          refresh_token,
+        };
+      }
     
 
 }
