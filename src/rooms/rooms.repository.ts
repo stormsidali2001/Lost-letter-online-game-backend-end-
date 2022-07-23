86a7e5ae1450d075bb8b1fa0d65c8d57521f9004
stormsidali2001 <assoulsidali@gmail.com>
2022-07-23 18:48:50 +0200

    createRoom

--- src/rooms/rooms.repository.ts
+++ src/rooms/rooms.repository.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable, Logger } from "@nestjs/common";
+import { Inject, Injectable, InternalServerErrorException, Logger } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import Redis from "ioredis";
 import { IoRedisKey } from "src/redis.module";
+import { CreateRoomDto } from "./room.dto";
 
 
 @Injectable()
@@ -13,6 +14,34 @@ export class RoomsRepository{
         configService:ConfigService
     ){
         this.ttl = configService.get("ROOM_TTL");
+    }
+    async createRoom(data:CreateRoomDto){
+        const {name,password,id} = data;
+        const initialRoom = {
+            name,
+            password,
+        };
+        this.logger.log(
+            `Creating new room: ${JSON.stringify(initialRoom)} with TTL ${
+              this.ttl
+            } ...`,
+          );
+          
+          const key = `room:${id}`;
+          try{
+               await this.redisClient
+              .multi([
+                    ['send_command', 'JSON.SET', key, '.', JSON.stringify(initialRoom)],
+                    ['expire', key, this.ttl],
+              ])
+              .exec();
+
+          }catch(e){
+            this.logger.error(
+                `Failed to create room ${JSON.stringify(initialRoom)}\n${e}`,
+              );
+              throw new InternalServerErrorException();
+          }
     }
    
 }
